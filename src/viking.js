3743b514d76cff4103f116a01394db6797c3b930
MijyPark <parkmijy2021@gmail.com>
2023-04-06 14:11:51 +0200

    vikingsvikings

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,47 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+      this.health = health;
+      this.strength = strength;
+    }
+   attack() {
+    return this.strength;
+
+}
+   receiveDamage(damage) {
+    this.damage = damage;
+    this.health  -= damage;
+   }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+  attack() { 
+    return this.strength;
+    } 
 
+  receivedDamage(damage) {
+    this.health = newHealth;
+    if (newHealth > this.damage) {
+        return `'${this.name} has received ${this.damage} points of damage.'`;
+    } else {
+        return `'${this.name} has died in act of combat.'`;
+    }
+   }
+  battleCry() {
+    return "Odin Owns You All!";
+   }
+}
 // Saxon
-class Saxon {}
+class Saxon extends Viking {
+  constructor(health, strength) {
+    super(name,health, strength);
+  }
+}
 
 // War
 class War {}
